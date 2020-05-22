2a0a620fe8fe0f914a6ccc696ed157b16e0eac7f
Khanh Tran <ankhanhct@gmail.com>
2020-05-23 01:40:26 +0700

    Initial Commit

---
+++ QuanRemiders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuanRemiders
+//
+//  Created by Tran An Khanh on 5/23/20.
+//  Copyright © 2020 Momo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
